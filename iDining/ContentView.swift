30884eb96eb570afbb720e8e343da7427cf8e7e4
A Ferrari <ferrarifam.christina@gmail.com>
2021-05-18 08:54:45 -0500

    have a nice day :)

---
+++ iDining/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  iDining
+//
+//  Created by Student on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            NavigationLink(destination: ItemDetail(item: item)) {
+                                ItemRow(item: item)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
